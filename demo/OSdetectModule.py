dd1b97e531493443e26686e89061dafa06d3bb71
然然今天吃什么捏 <83222283+Neko-chanQwQ@users.noreply.github.com>
2022-05-19 11:27:39 +0800

    Add files via upload

---
+++ demo/OSdetectModule.py
@@ -0,0 +1,74 @@
+import nmap
+import time
+import socket
+import FontColorModule
+from kamene.all import *
+
+
+lettercolor = FontColorModule.ChangeColor()
+
+
+class OsScan():
+    def __init__(self, ip):
+        self.ip = str(ip)
+
+    
+    def NmapScan(self):
+        nscan = nmap.PortScanner()    
+        try:
+            #print(self.ip)
+            result = nscan.scan(hosts = self.ip, arguments='-O')      #填写nmap参数
+            os = result["scan"][self.ip]['osmatch'][0]['name']      #切片提取操作系统数据
+            time.sleep(0.5)
+            gui.ui.outwindow.append(lettercolor.Res + '[+]Target:{0} OS:{1}(For reference only!!!)'.format(self.ip, os) + lettercolor.ColorEnd)
+            time.sleep(0.5)
+            gui.ui.outwindow.append(lettercolor.Mes + '[*]OsDetect Finished.' + ' >>> ' + \
+            time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + lettercolor.ColorEnd)
+            return
+        except:
+            gui.ui.outwindow.append(lettercolor.Warn + '[-]Invalid input or Connect timeout.' + ' >>> ' + \
+            time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + lettercolor.ColorEnd)
+            return
+
+    
+    def TtlScan(self):
+        pktip = kamene.all.IP(dst = self.ip)
+        icmp = kamene.all.ICMP()
+        packet = pktip / icmp
+        result = sr1(packet, timeout=3, verbose=0)	# 构造ping包向目标主机发送
+
+        if result is None:
+            pass
+        elif int(result['IP'].ttl) <= 64:    # 判断目标主机响应包中TTL值是否小于等于64
+            gui.ui.outwindow.append(lettercolor.Res + '[+]Target:{0} OS:Linux/Unix(For reference only!!!)'.format(self.ip) \
+            + lettercolor.ColorEnd)     # 是的话就为linux/Unix
+        else:
+            gui.ui.outwindow.append(lettercolor.Res + '[+]Target:{0} OS:Windows(For reference only!!!)'.format(self.ip) \
+            + lettercolor.ColorEnd)  # 反之就是Windows
+        
+        time.sleep(0.5)
+        gui.ui.outwindow.append(lettercolor.Mes + '[*]OsDetect Finished.' + ' >>> ' + \
+        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + lettercolor.ColorEnd)
+
+
+
+def Receive(Host, Method, GUI):
+    global host, gui, scanmethod
+    host = Host
+    scanmethod = Method
+    gui = GUI
+    return host, gui, scanmethod
+    
+
+def StartScan():
+    tranhost = socket.gethostbyname(host)
+    scan = OsScan(tranhost)
+    if str(scanmethod).upper() == 'TTL':
+        scan.TtlScan()
+    elif str(scanmethod).upper() == 'NMAP':
+        scan.NmapScan()
+    else:
+        gui.ui.outwindow.append(lettercolor.Warn + '[-]Invalid input ScanMethod.' + ' >>> ' + \
+        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + lettercolor.ColorEnd)
+    return
+
---
+++ demo/ScanModuletest.py
@@ -0,0 +1,68 @@
+import FontColorModule
+import socket
+import queue
+import threading
+import time
+
+
+#测试数据（从GUI中传入数据）
+meslist = []
+que = queue.Queue()         #初始化一个队列，用于保存扫描端口队列
+lettercolor = FontColorModule.ChangeColor()
+
+
+class PortScanner(threading.Thread):
+    def __init__(self, host):
+        super().__init__()
+        self.host = str(host)
+
+
+    def run(self) -> None:
+        while True:
+            port = que.get()
+            self.Scanner(port)
+            que.task_done()
+            time.sleep(0.2)
+
+
+    def Scanner(self, port):
+        conn = socket.socket()
+        try:
+            conn.connect((self.host, port))
+            #print(f'[+]Port{port}:Open.')
+            #gui.ui.outwindow.append(lettercolor.Res + f'[+]PortNum:{port} (Status:Open.)' + lettercolor.ColorEnd)
+            gui.ui.outwindow.append(lettercolor.Res + '[+]PortNum:{0} (Status:Open.)'.format(port) + lettercolor.ColorEnd)
+            conn.close()
+        except:
+            pass
+
+           
+def Receive(host, start, end, thread):      #接收外部数据函数
+    global meslist
+    meslist = [host, start, end, thread]
+    return meslist
+    
+
+def UIreceive(GUI):      #接收GUI实例
+    global gui
+    gui = GUI
+    return gui
+
+
+def StartScan():    #开始扫描函数
+    ip = socket.gethostbyname(meslist[0])
+    
+    for i in range(int(meslist[3])):
+        t = PortScanner(ip)
+        t.setDaemon(1)      #添加守护进程
+        t.start()
+        
+    for i in range(int(meslist[1]), int(meslist[2])+1):         #根据第二第三项输入构建队列，左闭右开区间，所以endport+1
+        que.put(i)
+
+    que.join()
+    gui.ui.outwindow.append(lettercolor.Mes + '[*]PortScan Finished.' + ' >>> ' + \
+    time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + lettercolor.ColorEnd)
+    #print(type(t))
+    #print(id(t))
+    
